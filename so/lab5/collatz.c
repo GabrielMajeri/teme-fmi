d352011cb28d5e8a2b614e17ee79e137bc112909
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-10-25 20:45:34 +0300

    Laboratorul 5 de sisteme de operare

---
+++ so/lab5/collatz.c
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/mman.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+const char COLLATZ_SHM_NAME[] = "/collatz_shm";
+
+#define CLEANUP_SHM \
+    close(shm_fd); \
+    shm_unlink(COLLATZ_SHM_NAME);
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        printf("Expected at least one number");
+        printf("Usage: %s n_1 ... n_k\n", argv[0]);
+        return 1;
+    }
+
+    int shm_fd = shm_open(COLLATZ_SHM_NAME, O_RDWR | O_CREAT | O_EXCL, S_IRWXU);
+    if (shm_fd < 0) {
+        perror("Failed to open shared memory handle");
+        return 1;
+    }
+
+    const int k = argc - 1;
+    const size_t page_size = getpagesize();
+
+    if (ftruncate(shm_fd, k * page_size) != 0) {
+        perror("Failed to allocate shared memory");
+        CLEANUP_SHM
+        return 1;
+    }
+
+    int parent_pid = getpid();
+    printf("Starting parent %d\n", parent_pid);
+
+    int* results = mmap(NULL, k * page_size, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    for (int i = 0; i < k; ++i) {
+        pid_t child_pid = fork();
+        if (child_pid < 0) {
+            perror("Failed to create child");
+            CLEANUP_SHM
+            return 1;
+        } else if (child_pid == 0) {
+            int* buf = results + i * page_size / sizeof(int);
+
+            if (buf == MAP_FAILED) {
+                perror("Failed to map memory");
+                return 1;
+            }
+
+            int value = 0;
+            sscanf(argv[1 + i], "%d", &value);
+
+            int len = 0;
+
+            if (value <= 1) {
+                return 1;
+            }
+
+            do {
+                buf[len++] = value;
+                if (value % 2 == 0) {
+                    value /= 2;
+                } else {
+                    value = value * 3 + 1;
+                }
+            } while (value != 1);
+            buf[len] = value;
+
+            buf[0] = len;
+
+            return 0;
+        }
+    }
+
+    for (int i = 0; i < k; ++i) {
+        int status = 0;
+        pid_t child_pid = wait(&status);
+        printf("Done child %d exited with code %d\n", child_pid, status);
+    }
+
+    for (int i = 0; i < k; ++i) {
+        int* buf = &results[i * page_size / 4];
+        int size = buf[0];
+        printf("%d\n", size);
+        for (int j = 1; j <= size; ++j) {
+            printf("%d ", buf[j]);
+        }
+        printf("\n");
+    }
+
+    munmap(results, k * page_size);
+
+    CLEANUP_SHM
+}
