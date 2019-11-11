2555b71d0666111e9f7ec90b453b36e0966d9b45
Gabriel Majeri <gabriel.majeri6@gmail.com>
2019-11-11 12:37:54 +0200

    Problema cu robotul de la programare dinamică

---
+++ tap/lab4/robot.py
@@ -0,0 +1,34 @@
+with open('robot.txt') as fin:
+    n, m = map(int, next(fin).split())
+
+    mat = [[int(x) for x in next(fin).split()] for _ in range(n)]
+
+sum_max = [[0 for _ in range(m)] for _ in range(n)]
+preds = [[(-1, -1) for _ in range(m)] for _ in range(n)]
+
+sum_max[0][0] = mat[0][0]
+preds[0][0] = (-1, -1)
+
+for i in range(n):
+    for j in range(m):
+        up = sum_max[i - 1][j]
+        left = sum_max[i][j - 1]
+        if up > left:
+            sum_max[i][j] = mat[i][j] + up
+            preds[i][j] = (i - 1, j)
+        else:
+            sum_max[i][j] = mat[i][j] + left
+            preds[i][j] = (i, j - 1)
+
+print(sum_max[-1][-1])
+
+current = (n - 1, m - 1)
+stack = []
+
+while current != (0, 0):
+    stack.append(current)
+    current = preds[current[0]][current[1]]
+
+stack.append(current)
+
+print(*reversed(stack), sep='\n')
