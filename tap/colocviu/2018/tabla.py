7e5d4043abf655ce7383a68becdb42364e1594fa
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-01-23 21:35:44 +0200

    Adaugă problema cu tabla

---
+++ tap/colocviu/2018/tabla.py
@@ -0,0 +1,49 @@
+"""
+Se dă o tablă de dimensiune 2^N * 2^N completată după următoarea regulă:
+
+ 1  3  9 11
+ 4  2 12 10
+13 15  5  7
+16 14  8  6
+
+Fiind dat un număr k, trebuie determinat rândul și coloana pe care se află
+în matrice.
+"""
+
+def find(dim, k):
+    if dim == 2:
+        if k == 1:
+            return (1, 1)
+        elif k == 2:
+            return (2, 2)
+        elif k == 3:
+            return (1, 2)
+        else:
+            return (2, 1)
+
+    half = dim // 2
+    num = dim ** 2
+
+    quarter = num // 4
+
+    if k <= quarter: # Stânga-sus
+        i, j = find(half, k)
+        return i, j
+    elif k <= 2 * quarter: # Dreapta-jos
+        i, j = find(half, k - quarter)
+        return i + half, j + half
+    elif k <= 3 * quarter: # Dreapta-sus
+        i, j = find(half, k - 2 * quarter)
+        return i, j + half
+    else: # Stânga-jos
+        i, j = find(half, k - 3 * quarter)
+        return i + half, j
+
+
+# Acestea ar fi citite din fișier
+n, k = 2, 12
+
+dim = 2 ** n
+i, j = find(dim, k)
+
+print(i, j)
---
+++ tap/colocviu/README.md
@@ -0,0 +1,6 @@
+# Rezolvări la exerciții din colocvii
+
+## Iarna 2018
+
+- [Robot](2018/robot.py) care trebuie să colecteze puncte.
+- [Tablă pătratică](2018/tabla.py), asemănător cu Z-parcurgerea.
