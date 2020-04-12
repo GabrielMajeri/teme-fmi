f1cfb65acedbff9f1a83880dc2c63a0b49939c99
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-04-12 22:13:44 +0300

    Șterge 99-problems pentru Prolog

--- pl/99-problems/01.pl
+++
@@ -1,7 +0,0 @@
-/* If the list has only one element, then that element is the last one. */
-last_element(X, [X]).
-
-/* If the list has more elements, then we skip the first element and
- * keep looking for the last one.
- */
-last_element(X, [Head|Tail]) :- last_element(X, Tail).
--- pl/99-problems/README.md
+++
@@ -1,3 +0,0 @@
-# Ninety-Nine Prolog Problems
-
-Rezolvarea explicată în Prolog a celor 99 de probleme [de pe acest site](https://sites.google.com/site/prologsite/prolog-problems).
