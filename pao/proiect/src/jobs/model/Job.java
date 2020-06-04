54012ef526af091d085eb93f78aaf79b9b0e14da
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-06-04 17:13:57 +0300

    Refactor Job class

--- pao/proiect/src/jobs/model/Job.java
+++ pao/proiect/src/jobs/model/Job.java
@@ -1,17 +1,62 @@
 package jobs.model;
 
+import csv.CsvTypeFactory;
 import jobs.utils.IdAllocator;
 
 import java.time.Instant;
 import java.util.Objects;
 
+/**
+ * A job offering posted by a company.
+ */
 public class Job {
-    private final static IdAllocator jobIds = new IdAllocator(1, 15000);
+    public final static CsvTypeFactory<Job> FACTORY = new CsvTypeFactory<Job>() {
+        @Override
+        public String[] getColumnNames() {
+            return new String[]{"id", "title", "timePosted", "category", "companyId"};
+        }
 
+        @Override
+        public String[] toStringArray(Job job) {
+            return new String[]{
+                    Integer.toString(job.id),
+                    job.title,
+                    job.timePosted.toString(),
+                    job.category.toString(),
+                    Integer.toString(job.companyId)
+            };
+        }
+
+        @Override
+        public Job fromStringArray(String[] data) {
+            int id = Integer.parseInt(data[0]);
+            String title = data[1];
+            Instant timePosted = Instant.parse(data[2]);
+            Category category = Category.valueOf(data[3]);
+            int companyId = Integer.parseInt(data[4]);
+            return new Job(id, title, timePosted, category, companyId);
+        }
+    };
+    private final static IdAllocator jobIds = new IdAllocator(1, 15000);
+    /**
+     * Unique identifier of the job.
+     */
     public final int id;
+    /**
+     * Human-readable description of the job position.
+     */
     public final String title;
+    /**
+     * The time it was first published.
+     */
     public final Instant timePosted;
+    /**
+     * The kind of job offered.
+     */
     public final Category category;
+    /**
+     * The unique identifier of the employer.
+     */
     public final int companyId;
 
     public Job(int id, String title, Instant timePosted, Category category, int companyId) {
