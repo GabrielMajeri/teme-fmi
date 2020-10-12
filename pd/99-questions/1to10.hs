a86a9842c86bceb17f99ab485eb29f3dba130daa
Gabriel Majeri <gabriel.majeri6@gmail.com>
2020-10-12 08:47:01 +0300

    Adaugă mai multe exerciții din 99 probleme de Haskell

--- pd/99-questions/1to10.hs
+++ pd/99-questions/1to10.hs
@@ -19,3 +19,30 @@ myButLast [] = undefined
 myButLast [_] = undefined
 myButLast [x, _] = x
 myButLast (_:restOfTheList) = myButLast restOfTheList
+
+
+-- Finds the k-th element of a list
+elementAt :: [a] -> Int -> a
+
+elementAt list index = list !! (index - 1)
+
+
+-- Finds the number of elements in a list
+myLength :: [a] -> Int
+
+myLength list = sum $ map (\_ -> 1) list
+
+
+-- Reverses a list.
+myReverse :: [a] -> [a]
+
+myReverse [] = []
+myReverse (x:xs) = (myReverse xs) ++ [x]
+
+
+-- Checks if a list is a palindrome.
+isPalindrome :: Eq a => [a] -> Bool
+
+isPalindrome [] = True
+isPalindrome [_] = True
+isPalindrome (x:xs) = (x == (last xs)) && (isPalindrome $ init xs)
