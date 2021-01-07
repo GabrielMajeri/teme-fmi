5e322a68f3f777a9252042c36d57707a1233ed45
Gabriel Majeri <gabriel.majeri6@gmail.com>
2021-01-07 08:51:43 +0200

    Rezolvă încă 2 exerciții din laboratorul 7 de SGBD

--- sgbd/laborator/lab7.sql
+++ sgbd/laborator/lab7.sql
@@ -39,6 +39,53 @@ END;
 /
 
 
+--- Exercițiul 2
+DECLARE
+    v_salariu emp.salary%TYPE;
+    v_nume NVARCHAR2(200);
+BEGIN
+    -- Citesc valoarea căutată a salariului
+    v_salariu := &salariu;
+
+    -- Caut angajatul cu salariul dat
+    SELECT first_name || ' ' || last_name AS nume
+    INTO v_nume
+    FROM emp
+    WHERE salary = v_salariu;
+
+    DBMS_OUTPUT.PUT_LINE('Angajat cu salariul ' || v_salariu || ': ' || v_nume);
+EXCEPTION
+    WHEN no_data_found THEN
+        DBMS_OUTPUT.PUT_LINE(
+            'Nu exista angajati care sa castige acest salariu'
+        );
+    WHEN too_many_rows THEN
+        DBMS_OUTPUT.PUT_LINE(
+            'Exista mai multi angajati care castiga acest salariu'
+        );
+END;
+/
+
+
+--- Exercițiul 3
+DECLARE
+    -- Atribui acestei erori un identificator unic.
+    integrity_constraint_violated EXCEPTION;
+    PRAGMA EXCEPTION_INIT(integrity_constraint_violated, -2292);
+BEGIN
+    -- Încerc să modific ID-ul unui departament care are angajați.
+    UPDATE departments
+    SET department_id = 101
+    WHERE department_id = 100;
+
+EXCEPTION
+    WHEN integrity_constraint_violated THEN
+        raise_application_error(-20000,
+            'Nu pot actualiza ID-ul departamentului');
+END;
+/
+
+
 --- Exercițiul 6
 DECLARE
     v_nume departments.department_name%TYPE;
